1fdd432228084c76602ec6311a83f8ef0a8f8025
Gls <guillaume.lesaint@studi.fr>
2025-12-02 19:11:23 +0100

    add pwa

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
     providers: [
         provideRouter([]),
         provideHttpClient(),
-        importProvidersFrom(BrowserAnimationsModule)
+        importProvidersFrom(BrowserAnimationsModule), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
     ]
 }).catch((err: unknown) => console.error(err));
 
